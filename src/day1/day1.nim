b2398d482736ce96b122df4d221e8692c8ea0801
Zander <steamwings@outlook.com>
2022-12-02 09:05:47 -0500

    day 1 part 2

--- src/day1/day1.nim
+++ src/day1/day1.nim
@@ -1,14 +1,39 @@
 import std/os
 import std/strutils
+import std/math
 
-var input = readFile(getCurrentDir() & "/src/day1/input.txt")
+var top = [0,0,0]
+var sum: int
 
-var cur_max, sum: int
-for line in input.split("\n"):
+proc challengeTheTop(value: int, top: var array[0..2, int]) =
+  # this is gross but oh well
+  if value > top[1]:
+    top[0] = top[1]
+    if value > top[2]:
+      top[1] = top[2]
+      top[2] = value
+    else:
+      top[1] = value
+  elif value > top[0]:
+    top[0] = value
+
+proc processLine(line: string) =
+  # echo "processing ", line
   if line == "":
-    cur_max = max(cur_max, sum)
+    challengeTheTop(sum, top)
     sum = 0
   else:
     sum = sum + parseInt(line)
+  # for v in top:
+  #   echo v
+  # echo "\n"  
 
-echo "answer: ", cur_max
+var input = readFile(getCurrentDir() & "/src/day1/input.txt")
+
+for line in input.split("\n"):
+  processLine(line)
+
+processLine("")
+
+echo "Top value: ", top[2]
+echo "Top 3 sum: ", sum(top)
