c988343713ec45d74f0f5279a1427b2ba8851287
Zander <steamwings@outlook.com>
2022-12-02 08:15:09 -0500

    day 1 part 1 solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # advent-of-code-22
 
 - [Install nim](https://nim-lang.org/install.html)
-- `nim c -r src/day1/day1.nim`
+- `nim c -o:bin/day1 -r src/day1/day1.nim`
--- src/day1/day1.nim
+++ src/day1/day1.nim
@@ -1,4 +1,14 @@
 import std/os
+import std/strutils
 
-var input = readFile(getCurrentDir() & "/src/day1/example_input.txt")
-echo input
+var input = readFile(getCurrentDir() & "/src/day1/input.txt")
+
+var cur_max, sum: int
+for line in input.split("\n"):
+  if line == "":
+    cur_max = max(cur_max, sum)
+    sum = 0
+  else:
+    sum = sum + parseInt(line)
+
+echo "answer: ", cur_max
