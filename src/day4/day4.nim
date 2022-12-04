1a836de45dcaaed95ce15c67a0f027f5dc711166
Zander <steamwings@outlook.com>
2022-12-04 07:06:02 -0500

    Day 4 part 2

--- src/day4/day4.nim
+++ src/day4/day4.nim
@@ -15,19 +15,25 @@ proc sign(n: int): Sign =
 proc oneIncludesTheOther(s1: Sign, s2: Sign): bool =
   return Zero in {s1, s2} or s1 != s2
 
-proc solvePart1(filename: string): int =
+proc solve(filename: string): (int,int) =
+  result = (0,0)
   for line in filename.lines:
     let numbers = line.split({',','-'}).map((n) => parseInt(n)) 
+    # Part 1
     if oneIncludesTheOther(
       sign(numbers[3] - numbers[1]), 
       sign(numbers[2] - numbers[0])):
-        inc(result)
-
-proc solvePart2(filename: string): int =
-  var f = filename.open()
-  while not f.endOfFile():
-    raise
+        inc(result[0])
+    # Part 2
+    var overlap_diff = 
+      if numbers[0] < numbers[2]:
+        numbers[2] - numbers[1]
+      else:
+        numbers[0] - numbers[3]
+    if overlap_diff <= 0:
+      inc(result[1])
     
-var filename = currentSourcePath.parentDir() & "/input.txt"
-echo "Part 1: ", solvePart1(filename)
-#echo "Part 2: ", solvePart2(filename)
+let filename = currentSourcePath.parentDir() & "/input.txt"
+let result = solve(filename)
+echo "Part 1: ", result[0]
+echo "Part 2: ", result[1]
