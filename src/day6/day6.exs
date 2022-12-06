4a162ac7e24e3a7eb986903147cfcd5d6665132b
Zander <steamwings@outlook.com>
2022-12-06 08:51:42 -0500

    day 6 part 1

--- src/day6/day6.exs
+++ src/day6/day6.exs
@@ -1,1 +1,26 @@
-IO.puts("running elixir")
+Mix.install([])
+
+defmodule Main do
+  defp all_uniq(enum) do
+    Enum.uniq(enum) === enum
+  end
+
+  defp magic({x, index}, acc) do
+    seq = Enum.take(acc,-3) ++ [x]
+    if length(seq) == 4 and all_uniq(seq) do
+      {:halt, index}
+    else
+      {[x], seq}
+    end
+  end
+
+  def run do
+    File.stream!("src/day6/input.txt", [], 1)
+    |> Stream.with_index(1)
+    |> Enum.flat_map_reduce([], &magic/2)
+    #|> Stream.run
+    |> elem(1)
+  end
+end
+
+IO.puts Main.run()
