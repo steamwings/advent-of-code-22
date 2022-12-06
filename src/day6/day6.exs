7d05df7e7c724515ddb3128f9909043842167ff2
Zander <steamwings@outlook.com>
2022-12-06 08:54:20 -0500

    day 6 part 2

--- src/day6/day6.exs
+++ src/day6/day6.exs
@@ -5,9 +5,10 @@ defmodule Main do
     Enum.uniq(enum) === enum
   end
 
-  defp magic({x, index}, acc) do
-    seq = Enum.take(acc,-3) ++ [x]
-    if length(seq) == 4 and all_uniq(seq) do
+  # Check the previous commit for part 1--I was busy today
+  defp magic2({x, index}, acc) do
+    seq = Enum.take(acc,-13) ++ [x]
+    if length(seq) == 14 and all_uniq(seq) do
       {:halt, index}
     else
       {[x], seq}
@@ -17,8 +18,7 @@ defmodule Main do
   def run do
     File.stream!("src/day6/input.txt", [], 1)
     |> Stream.with_index(1)
-    |> Enum.flat_map_reduce([], &magic/2)
-    #|> Stream.run
+    |> Enum.flat_map_reduce([], &magic2/2)
     |> elem(1)
   end
 end
