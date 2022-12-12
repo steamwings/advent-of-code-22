4a57d9f61fa10a5567e8d2a521abd94ca0b9ae3c
Zander <steamwings@outlook.com>
2022-12-12 00:42:06 -0500

    day 8 part 1 fix

--- src/day8/day8.exs
+++ src/day8/day8.exs
@@ -1,34 +1,43 @@
 defmodule Main do
 
-  def exploreRight(row, prevValue, maxLeft) do
-    [value | tail] = row
-    {count, maxRight} = cond do
-      tail == [] -> {0,0} # Base case for rightmost value
-      value > maxLeft -> exploreRight(tail, value, value)
-      true -> exploreRight(tail, value, maxLeft)
+  def exploreRight(row, maxLeft) do
+    [{value, counted} | tail] = row
+    {count, maxRight, row} = cond do
+      tail == [] -> {0,0,[]} # Base case for rightmost value
+      value > maxLeft -> exploreRight(tail, value)
+      true -> exploreRight(tail, maxLeft)
     end
 
-    maxRight = max(prevValue, maxRight)
-    count = if value > maxRight or value > maxLeft, do: count + 1, else: count
+    nowCounted = if counted == :false and (value > maxRight or value > maxLeft) do
+      true
+    else
+      false
+    end
+    count = if !counted and nowCounted, do: count + 1, else: count
+    maxRight = max(value, maxRight)
 
-    {count, maxRight}
+    {count, maxRight, [{value, nowCounted} | row]}
   end
 
   def part1() do
-    grid = File.stream!("src/day8/input.txt", [])
+    grid = File.stream!("src/day8/example.txt", [])
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.codepoints/1)
     |> Enum.to_list
+    |> Enum.map(fn x -> Enum.map(x, fn val -> {val, :false} end) end)
 
-    leftRightTotal = grid
-    |> exploreRight(0, 0)
-    |> elem(0)
+    {leftRightTotal, grid} = grid
+    |> Enum.map(&exploreRight(&1, 0))
+    |> Enum.reverse # Since we're about to manually construct a list which will reverse them back
+    |> Enum.reduce({0, []}, fn {count, _, row},{sum, grid} -> {sum + count, [row | grid]} end)
 
     upDownTotal = grid
     |> List.zip
     |> Enum.map(&Tuple.to_list/1)
-    |> exploreRight(0, 0)
-    |> elem(0)
+    |> Enum.map(&exploreRight(&1, 0))
+    #|> tap(&IO.puts/1)
+    |> Enum.map(fn {count, _, _} -> count end)
+    |> Enum.sum
 
     leftRightTotal + upDownTotal
   end
