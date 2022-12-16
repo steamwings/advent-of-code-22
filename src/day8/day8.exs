98b2982ea0c37da8cb966c7e8306105f6231c689
Zander <steamwings@outlook.com>
2022-12-15 21:33:29 -0500

    day 8 part 2

--- src/day8/day8.exs
+++ src/day8/day8.exs
@@ -19,14 +19,9 @@ defmodule Main do
     {count, maxRight, [{value, nowCounted} | row]}
   end
 
-  def part1() do
-    grid = File.stream!("src/day8/example.txt", [])
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.codepoints/1)
-    |> Enum.to_list
-    |> Enum.map(fn x -> Enum.map(x, fn val -> {val, :false} end) end)
-
+  def part1(grid) do
     {leftRightTotal, grid} = grid
+    |> Enum.map(fn x -> Enum.map(x, fn val -> {val, :false} end) end)
     |> Enum.map(&exploreRight(&1, 0))
     |> Enum.reverse # Since we're about to manually construct a list which will reverse them back
     |> Enum.reduce({0, []}, fn {count, _, row},{sum, grid} -> {sum + count, [row | grid]} end)
@@ -35,15 +30,61 @@ defmodule Main do
     |> List.zip
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.map(&exploreRight(&1, 0))
-    #|> tap(&IO.puts/1)
     |> Enum.map(fn {count, _, _} -> count end)
     |> Enum.sum
 
     leftRightTotal + upDownTotal
   end
 
+  def treesVisible(ahead, value) do
+    case ahead do
+      [height | tail] when height < value -> 1 + treesVisible(tail, value)
+      [] -> 0
+      _ -> 1
+    end
+  end
+
+  # rowAcc contains the scores; rowAhead
+  def exploreRight2(rowAcc, rowBehind) do
+    [{value, {top, bottom}} | tail] = rowAcc
+
+    {acc, rowAhead} = cond do
+      tail == [] -> {[], []} # Base case for rightmost value
+      true -> exploreRight2(tail, [value | rowBehind])
+    end
+
+    # Find scores
+    left = treesVisible(rowBehind, value)
+    right = treesVisible(rowAhead, value)
+
+    cond do
+      top == :empty -> {[{value, {left, right}} | acc], [value | rowAhead]} # first pass
+      true -> {[top * bottom * left * right | acc], [value | rowAhead]} # second pass
+    end
+  end
+
+  # There are many better ways of doing this...
+  def part2(grid) do
+    grid
+    |> Enum.map(fn x -> Enum.map(x, fn val -> {val, {:empty, :empty}} end) end)
+    |> Enum.map(&exploreRight2(&1, []))
+    |> Enum.map(fn {acc, _} -> acc end)
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&exploreRight2(&1, []))
+    |> Enum.map(fn {acc, _} -> acc end)
+    |> List.flatten
+    |> Enum.max
+  end
+
   def run() do
-    IO.puts "Part 1: #{part1()}"
+    grid = File.stream!("src/day8/input.txt", [])
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.codepoints/1)
+    |> Enum.to_list
+
+    IO.puts "Part 1: #{part1(grid)}"
+    IO.puts "Part 2: #{part2(grid)}"
   end
 end
 
